3ea068c03fca21b8e130e140362c7a5670f14872
Cauê Pereira Cermak <cauecermak@gmail.com>
2022-10-05 13:31:45 +0000

    inicializando e linkando arquivos (css/app/index/assets/GlobalStyle/etc)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+
+
+
+export default function App() {
+    return (
+        <>
+            foi
+        </>
+    );
+
+}
+
